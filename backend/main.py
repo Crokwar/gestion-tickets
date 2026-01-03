ba699fcea4d440eb2a0d085262a5ad9823855962
Crokwar <cristomar309@gmail.com>
2026-01-02 19:47:30 -0500

    fix: agregar URL de Vercelal CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,8 @@ async def read_root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "https://gestion-tickets-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
