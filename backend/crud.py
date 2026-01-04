d819b03a2e5d7de894bca942a125c519c15a7684
Crokwar <cristomar309@gmail.com>
2026-01-04 13:33:23 -0500

    feat: Agregar operacioness CRUD para tickets

--- backend/crud.py
+++ backend/crud.py
@@ -1,1 +1,70 @@
-# Operaciones CRUD
+from sqlalchemy.orm import Session
+from datatime import datatime
+import models
+import schemas
+
+def crear_tickets(db:Session, ticket:schemas.TicketCreate):
+    ahora = datatime.now()
+
+    db_ticket = models.Ticket(
+        codigo=ticket.codigo,
+        estado=ticket.estado or "en_proceso",
+        fecha=ahora.strftime("%Y-%m-%d"),
+        hora_creacion=ahora.strftime("%H:%M:%S"),
+        detalles=ticket.detalles
+    )
+
+    db.add(db_ticket)
+    db.commit()
+    db.refresh(db_ticket)
+
+    return db_ticket
+
+def obtener_tickets(db:Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Ticket)\
+        .order_by(models.Ticket.fecha.desc(), models.Ticket.hora_creacion.desc())\
+        .offset(skip)\
+        .limit(limit)\
+        .all()
+
+def obtener_tickets_por_fecha(db: Session, fecha: str):
+    return db.query(models.Ticket)\
+        .filter(models.Ticket.fecha == fecha)\
+        .order_by(models.Ticket.hora_creacion.desc())\
+        .all()
+
+
+def obtener_ticket_por_id(db:Session, ticket_id: int):
+    return db.query(models.Ticket)\
+        .filter(models.Ticket.id == id)\
+        .first()
+
+
+def actualizar_ticket(db:Session, ticket_id: int, ticket_update: schemas.TicketUpdate):
+    db_ticket = obtener_ticket_por_id(db, ticket_id)
+
+    if not db_ticket:
+        return None
+    
+    if ticket_update.estado is not None:
+        db_ticket.estado = ticket_update.estado
+
+    if ticket_update.detalles is not None:
+        db_ticket.detalles = ticket_update.detalles
+
+    db.commit()
+    db.refresh(db_ticket)
+
+    return db_ticket
+
+
+def eliminar_ticket(db:Session, ticket_id: int):
+    db_ticket = obtener_ticket_por_id(db, ticket_id)
+
+    if not db_ticket:
+        return False
+    
+    db.delete(db_ticket)
+    db.commit()
+
+    return True
