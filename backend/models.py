30093a0323333fc37d5858048a2029939db07320
Crokwar <cristomar309@gmail.com>
2026-01-04 12:19:34 -0500

    feat: Agregar modelo de Tickets

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,20 @@
-# Modelos SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Ticket(Base):
+
+    __tablename__ = "tickets"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    codigo = Column(String, index=True, nullable=False)
+
+    estado = Column(String, default="en_proceso", nullable=False)
+
+    fecha = Column(String, nullable=False)
+
+    hora_creacion = Column(String, nullable=False)
+
+    def __repr__(self):
+        #Así se verá representado el objeto
+        return f"<Ticket(id={self.id}, codigo={self.codigo}, estado={self.estado})"
